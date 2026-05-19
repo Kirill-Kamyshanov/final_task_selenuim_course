b81f5b726174ab4f21c4f55855295807da3e3da2
kamyshanov.k3 <kamyshanov.k3@rwb.ru>
2026-05-19 17:03:45 +0300

    added REAMDE.md

---
+++ README.md
@@ -0,0 +1,2 @@
+### Базовая команда для запуска:
+- pytest -v --tb=line --language=en test_main_page.py
--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element("css selector", "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element("css selector", "#login_link_invalid")
