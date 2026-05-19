aed44c227229b7b2be66ddfdd30a30252ba87d90
kamyshanov.k3 <kamyshanov.k3@rwb.ru>
2026-05-19 17:12:41 +0300

    changed pages classes and second test

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,5 +6,6 @@ class MainPage(BasePage):
         login_link = self.browser.find_element("css selector", "#login_link")
         login_link.click()
 
+
     def should_be_login_link(self):
-        self.browser.find_element("css selector", "#login_link_invalid")
+        assert self.is_element_present("css selector", "#login_link"), "Login link is not presented"
