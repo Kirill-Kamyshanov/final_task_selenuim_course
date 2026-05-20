deca6bc703e359b5fe1d035c5bb33e741e3c936f
kirill_kamyshanov <nestor10000@mail.ru>
2026-05-20 20:22:17 +0300

    small fix

--- test_product_page.py
+++ test_product_page.py
@@ -52,14 +52,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     """Проверка перехода на страницу для анрега"""
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, language):
     """Проверка отсутствия товаров в корзине для анрега"""
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
